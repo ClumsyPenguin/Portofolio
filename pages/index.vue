3f73e88e221cf71fd8263dc4c1e1c861706244fe
ClumsyPenguin <geerinckxseppe@gmail.com>
2020-08-24 09:45:54 +0200

    Init project v2.0

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+   
+    <NavBar>
+    </NavBar>
+    <div class="main-body">
+    <h1 class="home-title">I'm Seppe</h1>
+    <h2 class="home-title">FullStack Developer & Electronics Engineer</h2>
+    </div>
+  </div>
+</template>
+
+<style>
+  .home-title{
+    display: flex;
+    justify-content: center;
+    color: white;
+  }
+.background-image{
+  opacity: 0.3;
+}
+.main-body{
+  /*background-image: url('~/assets/back.jpg');*/
+  background-repeat: no-repeat;
+  height: 120%;
+  widows: 120%;
+}
+</style>
